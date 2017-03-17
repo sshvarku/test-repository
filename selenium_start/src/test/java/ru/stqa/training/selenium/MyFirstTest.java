2e5366185a2a356bb7f10ef7b14cae424b581b61
Sergey Shvarkunov <shvfor@gmail.com>
2017-03-17 17:47:26 +0300

    Д.З. Подготовьте инфраструктуру
    Здравствуйте! Вроде бы делал всё, как по интструкции. Но при запуске
    первого теста получаю ошибку:
    Error:(37, 13) java: cannot access com.google.common.base.Function
    class file for com.google.common.base.Function not found

---
+++ selenium_start/src/test/java/ru/stqa/training/selenium/MyFirstTest.java
@@ -0,0 +1,45 @@
+package ru.stqa.training.selenium;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.remote.RemoteWebDriver;
+//import com.google.common.base.Function;
+
+
+//import org.openqa.selenium.HasCapabilities;
+
+//import org.openqa.selenium.support.ui.WebDriverWait;
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class MyFirstTest {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start() {
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
+
+
+    @Test
+    public void myFirstTest() {
+        driver.get("http://www.google.com");
+        driver.findElement(By.name("q")).sendKeys("webdriver");
+        driver.findElement(By.name("btnG")).click();
+        wait.until(titleIs("webdriver - Поиск в Google"));
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
